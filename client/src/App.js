3c166b1c81b25fa053f398b06ea800cc0ee6d0e1
eunji Lee <eunjitech@gmail.com>
2021-09-03 18:30:24 +0900

    add logout form, auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LandingPage from "./componenets/views/LandingPage/LandingPage";
 import LoginPage from "./componenets/views/LoginPage/LoginPage";
 import RegisterPage from "./componenets/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
+      <Route exact path="/" component={Auth(LandingPage, null)} />
+      <Route path="/login" component={Auth(LoginPage, false)} />
+      <Route path="/register" component={Auth(RegisterPage, false)} />
     </Router>
   );
 }
